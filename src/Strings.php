082389dd19a3d0601d24c0915fbecbb8adbb1015
Steve Adams <irwtdvoys@gmail.com>
2020-11-11 22:22:51 +0000

    Correct isRegex to work with flags

--- src/Strings.php
+++ src/Strings.php
@@ -103,7 +103,7 @@
 
 		public static function isRegex(string $string): bool
 		{
-			return preg_match("/^\/[\s\S]+\/$/", $string) == 1 ? true : false;
+			return preg_match("/^\/[\s\S]+\/[gmixXsuUAJD]?$/", $string) == 1 ? true : false;
 		}
 
 		public static function isJson($value): bool
